ae8950a597d432d5ab44d83601887a078c31f630
ZubairSidd <zubairsidd20@gmail.com>
2021-04-01 14:34:42 +0530

    feat: play function
    the function will handle qna

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ var score = 0
 var userName = readline.question('What is your name? \n')
 log('Welcome ' + userName + ' to DoYouKnowZubair')
 log('------------------------')
+
+function play(question, answer)
+{
+  var userAnswer = readline.question(question + '\n')
+  if(userAnswer.toUpperCase() == answer.toUpperCase())
+  {
+    score++;
+    log('That is Correct')
+  }else
+  {
+    log('That is Wrong')
+  }
+  log('Your score is: ' + score)
+  log('------------------------')
+}
