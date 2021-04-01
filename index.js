8d5d45597022b4b50fdb01a505d52bf3955ce6a3
ZubairSidd <zubairsidd20@gmail.com>
2021-04-01 14:33:19 +0530

    feat: added welcome text
    welcome screen of quiz is done

---
+++ index.js
@@ -0,0 +1,7 @@
+const readline = require('readline-sync')
+const log = console.log
+
+var score = 0
+var userName = readline.question('What is your name? \n')
+log('Welcome ' + userName + ' to DoYouKnowZubair')
+log('------------------------')
